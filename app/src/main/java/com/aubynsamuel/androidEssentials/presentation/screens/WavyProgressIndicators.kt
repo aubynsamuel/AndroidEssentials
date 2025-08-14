c85daac160262e10e0d15389ae4aa3160772da48
De General <aubynsamuel05@gmail.com>
2025-08-14 05:48:45 +0000

    add color, wavelength and stroke to wavy indicators

--- app/src/main/java/com/aubynsamuel/androidEssentials/presentation/screens/WavyProgressIndicators.kt
+++ app/src/main/java/com/aubynsamuel/androidEssentials/presentation/screens/WavyProgressIndicators.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CircularWavyProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LinearWavyProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
@@ -24,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
@@ -64,14 +66,20 @@ fun WavyProgressIndicators() {
             CircularWavyProgressIndicator(
                 progress = { animatedProgress },
                 modifier = Modifier.size(200.dp),
-
-                )
+                color = MaterialTheme.colorScheme.error,
+                trackColor = MaterialTheme.colorScheme.primary,
+                stroke = Stroke(width = 30f),
+                trackStroke = Stroke(width = 30f),
+                wavelength = 30.dp
+            )
 
             LinearWavyProgressIndicator(
                 progress = { animatedProgress },
                 modifier = Modifier.fillMaxWidth(0.8f),
-
-                )
+                color = MaterialTheme.colorScheme.error,
+                trackColor = MaterialTheme.colorScheme.primary,
+                wavelength = 25.dp
+            )
 
             Slider(
                 value = progress,
