e9ae1129c923af5794f3be37f6da23fb99453830
De General <aubynsamuel05@gmail.com>
2025-08-14 11:52:32 +0000

    move nav list to its own file

---
+++ app/src/main/java/com/aubynsamuel/androidEssentials/presentation/navigation/NavList.kt
@@ -0,0 +1,46 @@
+package com.aubynsamuel.androidEssentials.presentation.navigation
+
+data class NavItem(val route: String, val name: String)
+
+val navList = listOf(
+    NavItem(
+        route = AppRoutes.TOP_HEADER_BAR,
+        name = "Top App Bar"
+    ),
+    NavItem(
+        route = AppRoutes.PULL_TO_REFRESH,
+        name = "Pull to refresh"
+    ),
+    NavItem(
+        route = AppRoutes.SEARCH_BAR,
+        name = "Search bar"
+    ),
+    NavItem(
+        route = AppRoutes.ANIMATED_CAROUSEL,
+        name = "Carousel"
+    ),
+    NavItem(
+        route = AppRoutes.BOTTOM_SHEET_SCAFFOLD,
+        name = "Bottom Sheet Scaffold"
+    ),
+    NavItem(
+        route = AppRoutes.BOTTOM_SHEET,
+        name = "Bottom Sheet"
+    ),
+    NavItem(
+        route = AppRoutes.SNACK_BAR,
+        name = "Snack Bar"
+    ),
+    NavItem(
+        route = AppRoutes.MOVING_GESTURE,
+        name = "Moving Gesture"
+    ),
+    NavItem(
+        route = AppRoutes.PROGRESS_INDICATORS,
+        name = "Progress Indicators"
+    ),
+    NavItem(
+        route = AppRoutes.BUTTONS,
+        name = "Buttons Screen"
+    ),
+)
