d1cce8eafbdadbd9bb92c6af9f1a2338e5f8c36c
De General <aubynsamuel05@gmail.com>
2025-08-14 11:51:42 +0000

    move app routes to its file

---
+++ app/src/main/java/com/aubynsamuel/androidEssentials/presentation/navigation/AppRoutes.kt
@@ -0,0 +1,15 @@
+package com.aubynsamuel.androidEssentials.presentation.navigation
+
+object AppRoutes {
+    const val PULL_TO_REFRESH = "pullToRefresh"
+    const val MAIN_SCREEN = "mainScreen"
+    const val SEARCH_BAR = "searchBar"
+    const val TOP_HEADER_BAR = "topHeaderBar"
+    const val ANIMATED_CAROUSEL = "animatedCarousel"
+    const val BOTTOM_SHEET_SCAFFOLD = "bottomSheetScaffold"
+    const val BOTTOM_SHEET = "bottomSheet"
+    const val SNACK_BAR = "snackBar"
+    const val MOVING_GESTURE = "movingGesture"
+    const val PROGRESS_INDICATORS = "progressIndicators"
+    const val BUTTONS = "buttons"
+}
--- app/src/main/java/com/aubynsamuel/androidEssentials/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/aubynsamuel/androidEssentials/presentation/navigation/Navigation.kt
@@ -18,20 +18,6 @@ import com.aubynsamuel.androidEssentials.presentation.screens.SnackBarScreen
 import com.aubynsamuel.androidEssentials.presentation.screens.TopApBarScreen
 import com.aubynsamuel.androidEssentials.presentation.screens.buttons.Buttons
 
-object AppRoutes {
-    const val PULL_TO_REFRESH = "pullToRefresh"
-    const val MAIN_SCREEN = "mainScreen"
-    const val SEARCH_BAR = "searchBar"
-    const val TOP_HEADER_BAR = "topHeaderBar"
-    const val ANIMATED_CAROUSEL = "animatedCarousel"
-    const val BOTTOM_SHEET_SCAFFOLD = "bottomSheetScaffold"
-    const val BOTTOM_SHEET = "bottomSheet"
-    const val SNACK_BAR = "snackBar"
-    const val MOVING_GESTURE = "movingGesture"
-    const val PROGRESS_INDICATORS = "progressIndicators"
-    const val BUTTONS = "buttons"
-}
-
 data class NavItem(val route: String, val name: String)
 
 val navList = listOf(
