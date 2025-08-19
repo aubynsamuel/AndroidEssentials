8971acc99c2c903a5caa27c3024d6b16ec69cc3b
De General <aubynsamuel05@gmail.com>
2025-08-19 18:37:14 +0000

    main screen improvements

--- app/src/main/java/com/aubynsamuel/androidEssentials/presentation/screens/MainScreen.kt
+++ app/src/main/java/com/aubynsamuel/androidEssentials/presentation/screens/MainScreen.kt
@@ -1,23 +1,27 @@
 package com.aubynsamuel.androidEssentials.presentation.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.LargeFlexibleTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -26,40 +30,45 @@ import com.aubynsamuel.androidEssentials.presentation.navigation.navList
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun MainScreen(navController: NavHostController) {
-    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
         topBar = {
-            LargeFlexibleTopAppBar(
+            TopAppBar(
                 title = { Text("Android UI Essentials") },
-                scrollBehavior = scrollBehavior,
-                titleHorizontalAlignment = Alignment.CenterHorizontally,
                 expandedHeight = 150.dp
             )
-        }, modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+        }
     ) { inner ->
-        Column(
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
             modifier = Modifier
+                .fillMaxSize()
                 .padding(inner)
-                .padding(10.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize(),
-                contentPadding = PaddingValues(10.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                items(navList) { navItem ->
 
-                    Button(onClick = {
-                        navController.navigate(
-                            navItem.route
+        ) {
+            itemsIndexed(navList) { index, navItem ->
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .clickable {
+                                navController.navigate(
+                                    navItem.route
+                                )
+                            }
+                            .width(180.dp)
+                            .height(100.dp)
+                            .padding(5.dp)
+                            .background(
+                                MaterialTheme.colorScheme.primary,
+                                shape = RoundedCornerShape(40.dp)
+                            )
+                    ) {
+                        Text(
+                            text = navItem.name,
+                            fontSize = 16.sp,
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colorScheme.onPrimary
                         )
-                    }) {
-                        Text(text = navItem.name, fontSize = 16.sp)
                     }
                 }
             }
