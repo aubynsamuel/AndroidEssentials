04b275f97943b4f6353248ac62099dca2636dbae
De General <aubynsamuel05@gmail.com>
2025-08-24 13:37:40 +0000

    update widget statement management and widget info xml file

--- app/src/main/java/com/aubynsamuel/androidEssentials/appWidget/MyAppWidget.kt
+++ app/src/main/java/com/aubynsamuel/androidEssentials/appWidget/MyAppWidget.kt
@@ -3,18 +3,20 @@ package com.aubynsamuel.androidEssentials.appWidget
 import android.content.Context
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.action.ActionParameters
 import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.action.ActionCallback
+import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.background
+import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
@@ -28,9 +30,10 @@ import androidx.glance.text.TextStyle
 import com.aubynsamuel.androidEssentials.MainActivity
 
 class MyAppWidget : GlanceAppWidget() {
+    val countKeyString = "count"
+    val countKey = intPreferencesKey(countKeyString)
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-
         provideContent {
             MaterialTheme {
                 MyContent()
@@ -40,7 +43,7 @@ class MyAppWidget : GlanceAppWidget() {
 
     @Composable
     private fun MyContent() {
-        var counter by remember { mutableIntStateOf(0) }
+        val count = currentState(key = countKey) ?: 0
         Column(
             modifier = GlanceModifier.Companion.fillMaxSize()
                 .background(MaterialTheme.colorScheme.primaryContainer).padding(16.dp),
@@ -55,19 +58,19 @@ class MyAppWidget : GlanceAppWidget() {
             )
 
             Text(
-                text = "Counter: $counter",
+                text = "Counter: $count",
                 modifier = GlanceModifier.Companion.padding(12.dp)
             )
 
             Row(horizontalAlignment = Alignment.Companion.CenterHorizontally) {
                 Button(
                     text = "Increment",
-                    onClick = { counter++ }
+                    onClick = actionRunCallback<Increment>()
                 )
                 Spacer(modifier = GlanceModifier.Companion.width(20.dp))
                 Button(
                     text = "Decrement",
-                    onClick = { counter-- }
+                    onClick = actionRunCallback<Decrement>()
                 )
             }
 
@@ -78,5 +81,41 @@ class MyAppWidget : GlanceAppWidget() {
                 onClick = actionStartActivity<MainActivity>()
             )
         }
+    }
+}
+
+class Increment() : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters,
+    ) {
+        updateAppWidgetState(context, glanceId) { prefs ->
+            val currentCount = prefs[MyAppWidget().countKey]
+            if (currentCount != null) {
+                prefs[MyAppWidget().countKey] = currentCount + 1
+            } else {
+                prefs[MyAppWidget().countKey] = 1
+            }
+        }
+        MyAppWidget().update(context, glanceId)
+    }
+}
+
+class Decrement() : ActionCallback {
+    override suspend fun onAction(
+        context: Context,
+        glanceId: GlanceId,
+        parameters: ActionParameters,
+    ) {
+        updateAppWidgetState(context, glanceId) { prefs ->
+            val currentCount = prefs[MyAppWidget().countKey]
+            if (currentCount != null) {
+                prefs[MyAppWidget().countKey] = currentCount - 1
+            } else {
+                prefs[MyAppWidget().countKey] = 0
+            }
+        }
+        MyAppWidget().update(context, glanceId)
     }
 }
